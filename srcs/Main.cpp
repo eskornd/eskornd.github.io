51c0a6f876f482c39e0bd4f599c6d6fdc918efa9
int255 <int255@outlook.com>
2020-02-02 10:15:10 +0800

    Update Main.cpp
    Added daily medium game

--- srcs/Main.cpp
+++ srcs/Main.cpp
@@ -232,7 +232,7 @@ int main()
         robot.solve(game, waitMS<5>);
     }
     
-    if (1)
+    if (0)
     {
         // Patrick game
         std::vector<Num> v={
@@ -255,6 +255,28 @@ int main()
         robot.solve(game, waitMS<5>);
     }
     
+    if (1)
+    {
+        // Medium game
+        std::vector<Num> v={
+            8,0,0,1,0,0,0,7,0,
+            0,2,0,0,4,0,8,0,0,
+            0,6,0,7,0,0,0,0,0,
+            0,0,0,4,7,0,9,0,8,
+            2,4,0,0,8,0,0,0,0,
+            0,3,8,0,0,0,0,0,5,
+            0,8,0,6,0,4,1,0,0,
+            9,0,0,0,0,7,2,0,4,
+            0,0,5,8,1,0,0,0,6,
+        };
+        Game<9> game(toGameInput<9>(v));
+        std::cout << game;
+        std::cout << "Medium game, press enter to start:" << std::endl;
+        waitInput();
+        Robot robot;
+        robot.setOrder(order);
+        robot.solve(game, waitMS<5>);
+    }
     if (0)
     {
         // Finnish Guy hard sudoku 2006
