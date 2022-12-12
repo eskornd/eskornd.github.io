e4fc4f3e22b9c6dfbe1457fcaf4608f0991bf4dd
int255 <int255@outlook.com>
2022-12-12 10:20:17 +0800

    updates

--- shapes/js/main.js
+++ shapes/js/main.js
@@ -16,15 +16,12 @@ async function go()
 	let elem = document.getElementById('Download');
 	let listener = elem.addEventListener('click',async ()=>{ 
 		try {
-			//download
-			// let utils = await editor.utils();
-			//let filePath = await utils.downloadAs({url:url});
 			const url1 = 'https://eskornd.github.io/shapes/assets/Can%2012%20oz.zae';
 			const url2 = 'https://eskornd.github.io/shapes/assets/Paint%20bucket_5L.zae';
-			// const url3 = 'https://speedtest-ca.turnkeyinternet.net/1000mb.bin'; // testing only
+			const url3 = 'https://gist.githubusercontent.com/khaykov/a6105154becce4c0530da38e723c2330/raw/41ab415ac41c93a198f7da5b47d604956157c5c3/gistfile1.txt';
 			
 			let win = await editor.currentWindow();
-			let ret = await win.endModal({assets: [url1, url2]});
+			let ret = await win.endModal({assets: [url1, url2, url3]});
 		} catch (err) {
 			alert('Exception: ' + err );
 		}
