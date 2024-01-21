a99cf853aaaf5d66d4f2001fe48c8483ca0be3f6
eskornd <nexu@esko.com>
2024-01-21 22:46:12 +0800

    updates

--- UECI/samplesite/annotations/js/main.js
+++ UECI/samplesite/annotations/js/main.js
@@ -7,8 +7,8 @@ import PageView from './PageView.js';
 console.log('main.js loaded');
 async function onInitialized()
 {
-	var appName = await ctx.editor.appName();
-	var verStr = await ctx.editor.versionString();
+	var appName = await ctx.editor.appName().catch((err)->{console.error(JSON.stringify(err));});
+	var verStr = await ctx.editor.versionString().catch((err)=>{console.error(JSON.stringify(err));});
 	ctx.view.setHostAppText(appName + ' ' + verStr);
 	var appInfo = await ctx.editor.appInfo();
 	ctx.view.setLanguageText(appInfo.language);
